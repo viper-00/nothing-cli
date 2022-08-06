2af7c89eecce1305c19c797ca28f98929f2a5ede
Viper <aur-014@hotmail.com>
2022-08-07 01:48:04 +0800

    fix grpc bug

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module nothing-cli
 go 1.18
 
 require (
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/fatih/color v1.13.0
+	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48
+)
+
+require (
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- protocol/grpc.go
+++ protocol/grpc.go
@@ -1,6 +1,7 @@
 package protocol
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"io"
@@ -8,16 +9,73 @@ import (
 	"strings"
 
 	"github.com/fatih/color"
+	"golang.org/x/net/http2"
 )
 
 const (
-	http2Preface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
+	http2HeaderLen = 9
+	http2Preface   = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
 )
 
 type GrpcInterop struct{}
 
 func (g *GrpcInterop) Dump(r io.Reader, source string, id int, quiet bool) {
 	g.readPrefect(r, source, id)
+
+	data := make([]byte, bufferSize)
+	for {
+		n, err := r.Read(data)
+		if n > 0 && !quiet {
+			var buf strings.Builder
+			buf.WriteString(color.HiGreenString("from %s [%d]\n", source, id))
+
+			var index int
+			for index < n {
+				frameInfo, moreInfo, offset := g.explain(data[index:n])
+				buf.WriteString(fmt.Sprintf("%s%s%s\n", color.HiBlueString("%s:(", grpcProtocol), color.HiYellowString(frameInfo), color.HiBlueString(")")))
+				end := index + offset
+				if end > n {
+					end = n
+				}
+				buf.WriteString(fmt.Sprint(hex.Dump(data[index:end])))
+				if len(moreInfo) > 0 {
+					buf.WriteString(fmt.Sprintf("\n%s\n\n", strings.TrimSpace(moreInfo)))
+				}
+				index += offset
+			}
+			display.PrintfWithTime("%s\n\n", strings.TrimSpace(buf.String()))
+		}
+		if err != nil && err != io.EOF {
+			fmt.Printf("unable to read data %v", err)
+		}
+		if n == 0 {
+			break
+		}
+	}
+}
+
+func (g *GrpcInterop) explain(b []byte) (string, string, int) {
+	if len(b) < http2HeaderLen {
+		return "", "", len(b)
+	}
+
+	frame, err := http2.ReadFrameHeader(bytes.NewReader(b[:http2HeaderLen]))
+	if err != nil {
+		return "", "", len(b)
+	}
+
+	frameLen := http2HeaderLen + int(frame.Length)
+	switch frame.Type {
+	case http2.FrameSettings:
+		switch frame.Flags {
+		case http2.FlagSettingsAck:
+			return "http2:settings:ack", "", frameLen
+		default:
+			return g.explainSettings(b[http2HeaderLen:frameLen]), "", frameLen
+		}
+	}
+
+	return "http2:" + strings.ToLower(frame.Type.String()), "", frameLen
 }
 
 func (g *GrpcInterop) readPrefect(r io.Reader, source string, id int) {
